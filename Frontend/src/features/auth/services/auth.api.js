56600e232939a9f2ae6903b491a4a52eecff317a
Ankit Kumar <144920510+ankityadav0011@users.noreply.github.com>
2026-07-11 20:00:15 +0530

    Update auth.api.js

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://interviewprep-ai-1-45lp.onrender.com",
     withCredentials: true
 })
 
